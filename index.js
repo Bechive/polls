f565850d0b3b06d00bda7b68e5c24dd12dc898ae
Benjamin Franzi <benjamin.franzi@outlook.com>
2016-09-18 14:26:16 +1000

    trying to heroku

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res) {
-  //res.sendFile(__dirname + '/public/index.html');
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
+  //res.send('Hello World!');
 });
 
 http.listen(3000, function() {
