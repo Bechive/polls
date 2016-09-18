3f428d9e67addea812348b7afe455958e567d502
Benjamin Franzi <benjamin.franzi@outlook.com>
2016-09-18 14:29:30 +1000

    jsut work!

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express');
+var app = express();
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-  //res.send('Hello World!');
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-http.listen(3000, function() {
-  console.log("listening on 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
+
